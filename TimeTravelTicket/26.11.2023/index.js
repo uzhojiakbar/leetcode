5224b9249b6674c5c551e7088db1c7eb8e7aa758
uzhojiakbar3 <uzhojiakbar3@gmail.com>
2023-11-26 09:58:17 +0500

    *70. Climbing Stairs - time travel ticket

---
+++ TimeTravelTicket/26.11.2023/index.js
@@ -0,0 +1,18 @@
+// *===============
+// *70. Climbing Stairs
+
+// https://leetcode.com/problems/climbing-stairs/?envType=study-plan-v2&envId=dynamic-programming
+
+var climbStairs = function (n) {
+  let res = [1, 1];
+
+  for (i = 2; i <= n; i++) {
+    res.push(res[i - 1] + res[i - 2]);
+    console.log(res);
+    console.log(i, "---", res[i - 1] + res[i - 2]);
+  }
+  console.log(res);
+  return res[n];
+};
+
+// console.log(climbStairs(10));
